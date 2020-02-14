f02e4a9e1bac4c8ff660b03d4a5fed97156595db
max <yd0095@gmail.com>
2020-02-14 16:59:57 +0900

    결과출력창 생성

--- KcpSampleSwifty/EndPageViewController.swift
+++ KcpSampleSwifty/EndPageViewController.swift
@@ -9,10 +9,51 @@
 import UIKit
 
 class EndPageViewController : UIViewController {
-
+    @IBOutlet weak var title1: UILabel!
+    @IBOutlet weak var title2: UILabel!
+    @IBOutlet weak var title3: UILabel!
+    @IBOutlet weak var title4: UILabel!
+    
+    @IBOutlet weak var result1: UILabel!
+    @IBOutlet weak var result2: UILabel!
+    @IBOutlet weak var result3: UILabel!
+    @IBOutlet weak var result4: UILabel!
+    
     var endList = [String : Any]()
     
     override func viewWillAppear(_ animated: Bool) {
          print(endList)
+        
+        if endList.count == 4 {
+            title1.text = "imp_uid"
+            result1.text = endList["imp_uid"] as? String
+            
+            title2.text = "merchant_uid"
+            result2.text = endList["merchant_uid"] as? String
+            
+            title3.text = "error_msg"
+            result3.text = endList["error_msg"] as? String
+      
+            title4.text = "imp_success"
+            result4.text = endList["imp_success"] as? String
+        }
+        else {
+            title1.text = "imp_uid"
+            result1.text = endList["imp_uid"] as? String
+          
+            title2.text = "merchant_uid"
+            result2.text = endList["merchant_uid"] as? String
+    
+            title3.text = "imp_success"
+            result3.text = endList["imp_success"] as? String
+            
+            title4.removeFromSuperview()
+            result4.removeFromSuperview()
+        }
     }
+    
+    
+    
+    
+    
 }
